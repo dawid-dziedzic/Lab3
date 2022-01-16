0c849f25b05d17b8705ac5694c7b5c2adf19d42b
Dawid Dziedzic <62702482+dawiddzie@users.noreply.github.com>
2022-01-16 18:59:32 +0100

    Add project files.

---
+++ zad2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using zad2.Models;
+
+namespace zad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Number")] Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ zad2/Models/Dane.cs
@@ -0,0 +1,8 @@
+﻿namespace zad2.Models
+{
+    public class Dane
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
